4fe515c98b9d633a69d17e3f809f6262bf8a56fd
David Torres <aimbot2001@gmail.com>
2022-12-21 22:14:47 +0700

    LastVersion+splashscreen

--- app/src/main/java/com/example/museumq/SplashScreen.java
+++ app/src/main/java/com/example/museumq/SplashScreen.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGHT = 3000;
@@ -12,12 +15,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
+        ImageView image =findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        image.startAnimation(animation);
         new Handler().postDelayed(new Runnable() {
 
             @Override
 
             public void run() {
+
 
                 Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
 
